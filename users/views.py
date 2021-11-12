29fe2e02fc040f0e532cf2d7c7267d9e65cf7e93
Emre <basarremre@outlook.com>
2021-11-13 02:24:33 +0300

    loginlogout

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm # we dont need it anymore 
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -12,13 +12,14 @@ def register(request):
         if form.is_valid():
             form.save() #if it is ok it will save it 
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created, you are now able to login!')
+            return redirect('login')
     else:
     # we will use classes already exists in django
         form  = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
         
-
-
+@login_required #add decorator here 
+def profile(request):
+    return render(request, 'users/profile.html')
 
