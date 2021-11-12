31ca2a93d25de02876ce9c0f9e6b82d1b5fe2203
Emre <basarremre@outlook.com>
2021-11-13 02:24:55 +0300

    newverions

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('blog.urls')),  #root loc of admin page
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
